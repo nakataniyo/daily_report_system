f4b53cab35a161241d6fc2dbff55ff8e22aa80fd
yo.nakatani <nakataniyo@hydrosoken.co.jp>
2022-09-20 18:58:09 +0900

    トップページアクションの作成

--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -17,14 +17,14 @@ import models.validators.ReportValidator;
 public class ReportService extends ServiceBase {
 
     /**
-     * 指定した従業員が作成した日報データを、指定されたページ数の一覧画面に表示する分取得しReportViewリストで返却する
+     * 指定した従業員が作成した日報データを、指定されたページ数の一覧画面に表示する分取得しReportViewのリストで返却する
      * @param employee 従業員
      * @param page ページ数
      * @return 一覧画面に表示するデータのリスト
      */
     public List<ReportView> getMinePerPage(EmployeeView employee, int page) {
 
-        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Report.class)
+        List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE, Report.class)
                 .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
                 .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
                 .setMaxResults(JpaConst.ROW_PER_PAGE)
@@ -33,7 +33,7 @@ public class ReportService extends ServiceBase {
     }
 
     /**
-     * 指定した従業員が作成した日報データ件数を取得し、返却する
+     * 指定した従業員が作成した日報データの件数を取得し、返却する
      * @param employee
      * @return 日報データの件数
      */
@@ -61,7 +61,7 @@ public class ReportService extends ServiceBase {
     }
 
     /**
-     * 日報テーブルのデータ件数を取得し、返却する
+     * 日報テーブルのデータの件数を取得し、返却する
      * @return データの件数
      */
     public long countAll() {
@@ -82,7 +82,7 @@ public class ReportService extends ServiceBase {
     /**
      * 画面から入力された日報の登録内容を元にデータを1件作成し、日報テーブルに登録する
      * @param rv 日報の登録内容
-     * @return バリテーションで発生したエラーのリスト
+     * @return バリデーションで発生したエラーのリスト
      */
     public List<String> create(ReportView rv) {
         List<String> errors = ReportValidator.validate(rv);
@@ -93,18 +93,18 @@ public class ReportService extends ServiceBase {
             createInternal(rv);
         }
 
-        //バリテーションで発生したエラーを返却（エラーがなければ0件のからリスト）
+        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
         return errors;
     }
 
     /**
      * 画面から入力された日報の登録内容を元に、日報データを更新する
      * @param rv 日報の更新内容
-     * @return バリテーションで発生したエラーのリスト
+     * @return バリデーションで発生したエラーのリスト
      */
     public List<String> update(ReportView rv) {
 
-        //バリテーションを行う
+        //バリデーションを行う
         List<String> errors = ReportValidator.validate(rv);
 
         if (errors.size() == 0) {
@@ -116,7 +116,7 @@ public class ReportService extends ServiceBase {
             updateInternal(rv);
         }
 
-        //バリテーションで発生したエラーを返却（エラーがなければ0件の空リスト）
+        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
         return errors;
     }
 
@@ -153,4 +153,5 @@ public class ReportService extends ServiceBase {
         em.getTransaction().commit();
 
     }
+
 }
